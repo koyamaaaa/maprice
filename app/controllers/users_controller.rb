adad5af0c6b9d0a96eb57364ec696681af8918b6
koyamaaaa <ykwm7.2.7@gmail.com>
2019-10-07 21:31:13 +0900

    add uesrs file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
   def signup
+    # ユーザー登録フォームを表示する
     render "/users/signup"
+  end
+
+  def create
+    # ユーザー登録フォームから値を取得してインスタンスにセットする
+    # インスタンスをモデルに登録する
+    # 登録した内容のユーザーページを表示する
+  end
+
+  def edit
+    # 該当のユーザーデータをフォームに反映する
+    # 該当のユーザーデータ内容を表示する
+  end
+
+  def update
+    # 変更したユーザーデータをフォームから取得してインスタンスにセットする
+    # 変更した内容でユーザーデータを更新する
+    # 変更した内容のユーザーページを表示する
+  end
+
+  def destroy
+    # 該当のユーザーを削除する
+    # ユーザー一覧画面を表示する
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # admin_menuルーティング
   scope '/admin_menu' do
     resources :plans, :providers, :net_lines,
               :services, :devices, :data_capacities,
-              :campaigns
+              :campaigns, :users
   end
 
   resources :plans, module: 'public', only: :index do
