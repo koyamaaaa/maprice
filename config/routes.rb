a63f4e913b4792af8906ec7f5622492bfd199f5b
koyamaaaa <ykwm7.2.7@gmail.com>
2019-10-14 23:48:15 +0900

    fix root-view to public/plans#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get '/', to: 'public/plans#index'
+
   # admin_menuルーティング
   scope '/admin_menu' do
     get '/users/', to: 'users#index'
