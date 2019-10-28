70f041a87b644725237083448067f4bdf271af9a
koyamaaaa <ykwm7.2.7@gmail.com>
2019-10-28 22:25:33 +0900

    fix search func

--- app/controllers/public/plans_controller.rb
+++ app/controllers/public/plans_controller.rb
@@ -21,9 +21,8 @@ class Public::PlansController < ApplicationController
   def search_params
     # requireの中にはモデル名を入れる
     # permitの中にはモデル名の中で欲しいカラムだけを入れる
-    params.require(:search_plan_form).permit(:device_id, :service_id, :provider_id,
-                                             :net_line_id, :data_capacity_id, :campaign_id
-                                            )
+    params.require(:search_plan_form).permit( :provider_id, :net_line_id, :data_capacity_id, 
+                                              service_ids: [], campaign_ids: [], device_ids: [])
   end
 
   # サービスに属する回線リストの取得
