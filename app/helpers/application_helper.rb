7b10e5dc919578faf3665ffb9f81b130eb283e72
koyamaaaa <ykwm7.2.7@gmail.com>
2019-12-16 23:17:04 +0900

    fix-metatags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title: "maprice",
+      description: "価格の地図",
+      noindex: ! Rails.env.production?,
+      charset: "UTF-8",
+      og: {
+        title: :title,
+        description: :description,
+        type: "website",
+        url: request.original_url,
+        site_name: "maprice",
+        local: "ja_JP"
+      },
+      twitter: {
+        card: "summary"
+      }
+    }
+  end
 end
