8ee1e1b0b67b044e56e5b3927f62a1f76c211e56
koyamaaaa <ykwm7.2.7@gmail.com>
2019-12-30 17:49:25 +0900

    fix puma

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -16,6 +16,6 @@ workers 2 # 起動数
 preload_app!
 
 # デーモンとして起動
-daemonize
+# daemonize
 # 標準出力と標準エラー出力をファイルに出力
 stdout_redirect "#{tmp_path}/logs/puma.stdout.log", "#{tmp_path}/logs/puma.stderr.log", true
